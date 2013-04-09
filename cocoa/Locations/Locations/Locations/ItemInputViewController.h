846ce56d0ec9b02f87944e98f8d86f34980ed067
varjunan <varjunan@loaner5-lm.corp.yahoo.com>
2013-04-09 16:56:41 -0700

    add more UI components

--- cocoa/Locations/Locations/Locations/ItemInputViewController.h
+++ cocoa/Locations/Locations/Locations/ItemInputViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AddressBookUI/AddressBookUI.h>
 
 @protocol ContactAddDelegate;
 
@@ -17,22 +18,40 @@
 	NSString *lastName;
 	NSString *initialPoints;
     NSString *notes;
+    NSString *phoneNo;
+    NSString *emailAddr;
+    NSDate *eventDate;
 }
 
 @property (nonatomic, retain) NSString * firstName;
 @property (nonatomic, retain) NSString * lastName;
 @property (nonatomic, retain) NSString * initialPoints;
 @property (nonatomic, retain) NSString * notes;
+@property (nonatomic, retain) NSString * phoneNo;
+@property (nonatomic, retain) NSString * emailAddr;
+@property (nonatomic, retain) NSDate* eventDate;
 
 @end
 
-@interface ItemInputViewController : UIViewController {
+@interface ItemInputViewController : UIViewController <ABPeoplePickerNavigationControllerDelegate>{
     ContactData *contactData;
  }
 
-@property (nonatomic,retain) ContactData *contactData;
+@property (weak, nonatomic) IBOutlet UITextField *phoneNo;
+@property (weak, nonatomic) IBOutlet UITextField *firstName;
+@property (weak, nonatomic) IBOutlet UITextField *lastName;
+@property (weak, nonatomic) IBOutlet UITextField *notes;
+@property (weak, nonatomic) IBOutlet UITextField *initialPoints;
+@property (weak, nonatomic) IBOutlet UITextField *emailAddr;
+@property (weak, nonatomic) IBOutlet UITextField *eventDate;
+- (IBAction)showPicker:(id)sender;
+- (IBAction)saveContact:(id)sender;
+- (IBAction)cancel:(id)sender;
+- (IBAction)selectDate:(id)sender;
 
+@property (nonatomic,retain) ContactData *contactData;
 @property (nonatomic, assign) id<ContactAddDelegate> delegate;
+@property (nonatomic, retain) UIDatePicker *datePickerView;
 
 @end
 
--- cocoa/Locations/Locations/Locations/ItemInputViewController.m
+++ cocoa/Locations/Locations/Locations/ItemInputViewController.m
@@ -9,24 +9,18 @@
 #import "ItemInputViewController.h"
 
 @interface ItemInputViewController ()
-@property (weak, nonatomic) IBOutlet UITextField *firstName;
-@property (weak, nonatomic) IBOutlet UITextField *lastName;
-- (IBAction)saveContact:(id)sender;
-- (IBAction)cancel:(id)sender;
-@property (weak, nonatomic) IBOutlet UITextField *notes;
-@property (weak, nonatomic) IBOutlet UITextField *initialPoints;
 @end
 
 @implementation ContactData
 
-@synthesize firstName,lastName,initialPoints,notes;
+@synthesize firstName,lastName,initialPoints,notes,phoneNo,emailAddr,eventDate;
 
 @end
 
 @implementation ItemInputViewController
 
 @synthesize contactData;
-@synthesize delegate;
+@synthesize delegate,datePickerView;
 
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -85,9 +79,91 @@
     else if (theTextField == self.notes) {
         [theTextField resignFirstResponder];
     }
+    else if (theTextField == self.emailAddr) {
+        [theTextField resignFirstResponder];
+    }
+    else if (theTextField == self.eventDate) {
+        [theTextField resignFirstResponder];
+    }
+    else if (theTextField == self.phoneNo) {
+        [theTextField resignFirstResponder];
+    }
     return YES;
 }
 
+- (IBAction)showPicker:(id)sender {
+    ABPeoplePickerNavigationController *picker =
+    [[ABPeoplePickerNavigationController alloc] init];
+    picker.peoplePickerDelegate = self;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)peoplePickerNavigationControllerDidCancel:
+(ABPeoplePickerNavigationController *)peoplePicker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (BOOL)peoplePickerNavigationController:
+(ABPeoplePickerNavigationController *)peoplePicker
+      shouldContinueAfterSelectingPerson:(ABRecordRef)person {
+    [self displayPerson:person];
+    [self dismissViewControllerAnimated:YES completion:nil];
+    return NO;
+}
+- (BOOL)peoplePickerNavigationController:
+(ABPeoplePickerNavigationController *)peoplePicker
+      shouldContinueAfterSelectingPerson:(ABRecordRef)person
+                                property:(ABPropertyID)property
+                              identifier:(ABMultiValueIdentifier)identifier
+{
+    return NO;
+}
+
+- (void)displayPerson:(ABRecordRef)person
+{
+    NSString* name = (__bridge_transfer NSString*)ABRecordCopyValue(person, kABPersonFirstNameProperty);
+    self.firstName.text = name;
+    name = (__bridge_transfer NSString*)ABRecordCopyValue(person,kABPersonLastNameProperty);
+    self.lastName.text = name;
+    
+    NSString* phone = nil;
+    ABMultiValueRef phoneNumbers = ABRecordCopyValue(person,kABPersonPhoneProperty);
+    if (ABMultiValueGetCount(phoneNumbers) > 0) {
+        phone = (__bridge_transfer NSString*)
+        ABMultiValueCopyValueAtIndex(phoneNumbers, 0);
+    } else {
+        phone = @"[None]";
+    }
+    self.phoneNo.text = phone;
+    CFRelease(phoneNumbers);
+
+
+    NSString* email = nil;
+    ABMultiValueRef emails = ABRecordCopyValue(person,kABPersonEmailProperty);
+    if (ABMultiValueGetCount(emails) > 0) {
+        email = (__bridge_transfer NSString*)
+        ABMultiValueCopyValueAtIndex(emails, 0);
+    } else {
+        email = @"[None]";
+    }
+    self.emailAddr.text = email;
+    CFRelease(emails);
+
+}
+
+- (IBAction)selectDate:(id)sender {
+
+    //datePickerView = [[UIDatePicker alloc] initWithFrame:CGRectZero]; CGRectMake(0.0, 0.0, 320.0, 120.0)
+    datePickerView = [[UIDatePicker alloc] initWithFrame:CGRectMake(0.0, 100.0, 160.0, 80.0)];
+    datePickerView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
+    datePickerView.datePickerMode = UIDatePickerModeDate;
+    
+    
+    datePickerView.hidden = NO;
+    
+    [self.view addSubview:datePickerView];
+
+}
 @end
 
 
